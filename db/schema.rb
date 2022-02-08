507c21deae4c84692c33adcd150b21f2c29d78d1
johnmccann1991 <john.mccann@kellogg.northwestern.edu>
2022-02-08 01:52:31 +0000

    Final

--- db/schema.rb
+++ db/schema.rb
@@ -13,20 +13,24 @@
 ActiveRecord::Schema.define(version: 2022_02_07_041132) do
 
   create_table "movies", force: :cascade do |t|
-    t.string "movie_title"
-    t.string "year_released"
-    t.string "MPAA_rating"
-    t.string "director"
+    t.string "title"
+    t.integer "year_released"
+    t.string "rated"
+    t.integer "person_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "people", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "roles", force: :cascade do |t|
+    t.integer "movie_id"
+    t.integer "person_id"
+    t.string "character_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- kmdb.rb
+++ kmdb.rb
@@ -78,18 +78,13 @@
 # TODO!
 
 # Prints a header for the movies output
-puts "Movies"
-puts "======"
-puts ""
+
 
 # Query the movies data and loop through the results to display the movies output
 # TODO!
 
 # Prints a header for the cast output
-puts ""
-puts "Top Cast"
-puts "========"
-puts ""
+
 
 # Query the cast data and loop through the results to display the cast output for each movie
 # TODO!
@@ -258,6 +253,30 @@ role15.character_name = "Selina Kyle"
 role15.movie_id = movie3.id
 role15.person_id = person12.id
 role15.save
+
+movies=Movie.all
+roles=Role.all
+
+puts "Movies"
+puts "======"
+puts ""
+
+for movie in movies
+    director = Person.where({id: movie.person_id}) [0]
+    puts "#{movie.title} #{movie.year_released} #{movie.rated} #{director.name}"
+end
+
+puts ""
+puts "Top Cast"
+puts "========"
+puts ""
+
+for role in roles
+    movie = Movie.where({id: role.movie_id}) [0]
+    person = Person.where({id: role.person_id}) [0]
+    puts "#{movie.title} #{person.name} #{role.character_name}" 
+end
+
 
 
 
